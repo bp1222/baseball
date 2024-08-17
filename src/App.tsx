eee0bdbe0e85cebc8b254fb0785b879c4257a47c
David Walker <dave@mudsite.com>
2024-08-17 10:19:30 -0600

    fixup

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,9 @@ function App() {
   return (
     <Routes>
       <Route
-        path="/baseball/"
+        path="/"
         element={
-          <Container fixed={true} maxWidth="md">
+          <Container maxWidth="md">
             <Header />
             <TeamSchedule />
           </Container>
