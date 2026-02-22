40f70d5acb61cfa2ef2fd09eb2e1cec5c326d3b1
Dave Walker <dave@mudsite.com>
2026-02-22 14:44:08 -0700

    format

--- src/components/modals/AppModals.tsx
+++ src/components/modals/AppModals.tsx
@@ -3,10 +3,10 @@
  * Each modal is in its own file under modals/ for clarity.
  */
 
-import {useModalState} from '@/context/ModalContext'
+import { useModalState } from '@/context/ModalContext'
 
-import {BoxscoreModal} from './BoxscoreModal'
-import {PlayerModal} from './PlayerModal'
+import { BoxscoreModal } from './BoxscoreModal'
+import { PlayerModal } from './PlayerModal'
 
 /**
  * Renders the modal stack (e.g. boxscore with player on top).
@@ -29,4 +29,3 @@ export const AppModals = () => {
     </>
   )
 }
-
